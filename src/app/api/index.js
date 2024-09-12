e0938b70c3890ff398b5f718a1edff929594ace2
Sabrina Denobile <masa@MacBook-Air-de-Sabrina.local>
2024-09-12 11:16:47 -0300

    exclusao de arquivos de produtos que nao é mais utilizado, organizacao de pastas para o componente pages onde se localiza o arquivo de produtos, exclusao de mais comentarios, criacao da pasta pages e styled para melhor organizacao dos arquivos

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -12,14 +12,3 @@ export const fetchProducts = async () => {
     return []; // Retorna um array vazio em caso de erro
   }
 };
-
-// Função para buscar um produto por ID
-export const fetchProductById = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    return response.data; // A resposta já é uma lista de produtos
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    return []; // Retorna um array vazio em caso de erro
-  }
-};
